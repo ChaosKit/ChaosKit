f2f48ac85cd1b7e1660c5c7515a3fa7f77f24cb9
Rafał Hirsz <rafal@hirsz.co>
2018-09-10 19:04:02 +0200

    Improve GLToneMapper to work in more situations
    I learned that outputting a vec3 color might yield artifacts interfering with the clearing color. It took a great while to debug this…
    
    This also contains support for setting tonemapping parameters: gamma, exposure and vibrancy.

--- ui/GLToneMapper.cpp
+++ ui/GLToneMapper.cpp
@@ -23,11 +23,11 @@ const char *FRAGMENT_SHADER = R"XD(
 #version 150
 
 in vec2 uv;
-out vec3 outColor;
+out vec4 outColor;
 uniform sampler2D histogram;
-uniform float gamma = 2.2;
-uniform float exposure = 0.0;
-uniform float vibrancy = 0.0;
+uniform float gamma;
+uniform float exposure;
+uniform float vibrancy;
 
 const float LOG10 = 2.302585092994046;
 const float E = 2.718281828459045;
@@ -49,22 +49,31 @@ void main()
 
     vec4 nonvibrantPoint = scaledPoint * gammaScale;
 
-    outColor =
+    vec3 newColor =
         (nonvibrantPoint + (vibrantPoint - nonvibrantPoint) * vibrancy).xyz;
+    outColor = vec4(newColor, 1.0);
 }
 )XD";
 
 }  // namespace
+
+void GLToneMapper::getUniformLocation(const char *name, GLuint *output) {
+  int location = program_->uniformLocation(name);
+  if (location < 0) {
+    qWarning() << "Invalid location for uniform:" << name;
+  } else {
+    *output = static_cast<GLuint>(location);
+  }
+}
 
 void GLToneMapper::initializeGL() {
   initializeOpenGLFunctions();
 
   // Setup the rectangle buffer
   glGenBuffers(1, &rectBuffer_);
-  glGenVertexArrays(1, &rectArray_);
+
   static float rectVertices[] = {-1.0f, -1.0f, -1.0f, 1.0f,
                                  1.0f,  1.0f,  1.0f,  -1.0f};
-  glBindVertexArray(rectArray_);
   glBindBuffer(GL_ARRAY_BUFFER, rectBuffer_);
   glBufferData(GL_ARRAY_BUFFER, sizeof(rectVertices), rectVertices,
                GL_STATIC_DRAW);
@@ -77,26 +86,47 @@ void GLToneMapper::initializeGL() {
   program_->addShaderFromSourceCode(QOpenGLShader::Vertex, VERTEX_SHADER);
   program_->addShaderFromSourceCode(QOpenGLShader::Fragment, FRAGMENT_SHADER);
   program_->link();
-  positionAttribute_ = program_->attributeLocation("position");
+
+  getUniformLocation("gamma", &gammaLocation_);
+  getUniformLocation("exposure", &exposureLocation_);
+  getUniformLocation("vibrancy", &vibrancyLocation_);
+  auto positionAttribute =
+      static_cast<GLuint>(program_->attributeLocation("position"));
+
+  glGenVertexArrays(1, &rectArray_);
+  glBindVertexArray(rectArray_);
+  glEnableVertexAttribArray(positionAttribute);
+  glVertexAttribPointer(positionAttribute, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
 }
+
+void GLToneMapper::setGamma(float gamma) { gamma_ = gamma; }
+
+void GLToneMapper::setExposure(float exposure) { exposure_ = exposure; }
+
+void GLToneMapper::setVibrancy(float vibrancy) { vibrancy_ = vibrancy; }
 
 void GLToneMapper::syncBuffer(const core::HistogramBuffer &buffer) {
   glBindTexture(GL_TEXTURE_2D, histogramTexture_);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, buffer.width(), buffer.height(), 0,
-               GL_RGBA, GL_FLOAT, buffer.data());
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, buffer.width(), buffer.height(), 0,
+               GL_RGBA, GL_FLOAT, buffer.data());
 }
 
 void GLToneMapper::map() {
+  glClearColor(0.f, 0.f, 0.f, 1.f);
   glClear(GL_COLOR_BUFFER_BIT);
 
   program_->bind();
+  program_->setUniformValue(gammaLocation_, gamma_);
+  program_->setUniformValue(exposureLocation_, exposure_);
+  program_->setUniformValue(vibrancyLocation_, vibrancy_);
+  glBindVertexArray(rectArray_);
+  glBindBuffer(GL_ARRAY_BUFFER, rectBuffer_);
   glBindTexture(GL_TEXTURE_2D, histogramTexture_);
-  glEnableVertexAttribArray(positionAttribute_);
-  glVertexAttribPointer(positionAttribute_, 2, GL_FLOAT, GL_FALSE, 0, 0);
   glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-  glDisableVertexAttribArray(positionAttribute_);
   program_->release();
 }
 
--- ui/GLToneMapper.h
+++ ui/GLToneMapper.h
@@ -11,9 +11,12 @@ namespace ui {
 class GLToneMapper : public QObject, protected QOpenGLFunctions_3_2_Core {
   Q_OBJECT
  public:
-  explicit GLToneMapper(QObject *parent): QObject(parent) {}
+  explicit GLToneMapper(QObject *parent = nullptr): QObject(parent) {}
 
   void initializeGL();
+  void setGamma(float gamma);
+  void setExposure(float exposure);
+  void setVibrancy(float vibrancy);
   void syncBuffer(const core::HistogramBuffer &buffer);
   void map();
 
@@ -22,7 +25,14 @@ class GLToneMapper : public QObject, protected QOpenGLFunctions_3_2_Core {
   GLuint rectBuffer_ = 0;
   GLuint rectArray_ = 0;
   GLuint histogramTexture_ = 0;
-  int positionAttribute_ = 0;
+  GLuint gammaLocation_ = 0;
+  GLuint exposureLocation_ = 0;
+  GLuint vibrancyLocation_ = 0;
+  float gamma_ = 2.2f;
+  float exposure_ = 0.f;
+  float vibrancy_ = 0.f;
+
+  void getUniformLocation(const char *name, GLuint *output);
 };
 
 }  // namespace ui
