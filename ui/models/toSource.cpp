5ab9a45c742e499e0c4915a9cf4fdff251185ad1
Rafał Hirsz <rafal@hirsz.co>
2019-02-13 21:13:01 +0100

    Add toSource implementations for UI models

---
+++ ui/models/toSource.cpp
@@ -0,0 +1,54 @@
+#include "toSource.h"
+#include <library/util.h>
+
+namespace chaoskit {
+namespace ui {
+
+ast::Blend toSource(const Blend *blend) {
+  if (blend == nullptr) {
+    return ast::Blend();
+  }
+
+  std::vector<ast::WeightedFormula> weighted_formulas;
+  weighted_formulas.reserve(static_cast<size_t>(blend->formulas().size()));
+  for (const auto &formula : blend->formulas()) {
+    weighted_formulas.push_back(toSource(formula));
+  }
+
+  return ast::Blend{std::move(weighted_formulas), toSource(blend->pre()),
+                    toSource(blend->post())};
+}
+
+ast::WeightedFormula toSource(const Formula *formula) {
+  return ast::WeightedFormula{library::createFormula(formula->type())->source(),
+                              formula->weightX(), formula->weightY()};
+}
+
+ast::Transform toSource(const QTransform &transform) {
+  return ast::Transform{
+      static_cast<float>(transform.m11()), static_cast<float>(transform.m21()),
+      static_cast<float>(transform.m31()), static_cast<float>(transform.m12()),
+      static_cast<float>(transform.m22()), static_cast<float>(transform.m32())};
+}
+
+ast::System toSource(const models::System *system) {
+  // Generate a monotonically increasing vector of limits
+  std::vector<float> limits(static_cast<size_t>(system->blends().size()));
+  std::transform(system->blends().begin(), system->blends().end(),
+                 limits.begin(), std::mem_fn(&Blend::weight));
+  std::partial_sum(limits.begin(), limits.end(), limits.begin());
+
+  // Combine blends and limits into LimitedBlends
+  std::vector<ast::LimitedBlend> limitedBlends;
+  limitedBlends.reserve(static_cast<size_t>(system->blends().size()));
+  std::transform(system->blends().begin(), system->blends().end(),
+                 limits.begin(), std::back_inserter(limitedBlends),
+                 [](const Blend *blend, float limit) {
+                   return ast::LimitedBlend{toSource(blend), limit};
+                 });
+
+  return ast::System{std::move(limitedBlends), toSource(system->finalBlend())};
+}
+
+}  // namespace ui
+}  // namespace chaoskit
---
+++ ui/models/toSource.h
@@ -0,0 +1,24 @@
+#ifndef CHAOSKIT_UI_TOSOURCE_H
+#define CHAOSKIT_UI_TOSOURCE_H
+
+#include <ast/Blend.h>
+#include <ast/System.h>
+#include <ast/Transform.h>
+#include <ast/WeightedFormula.h>
+#include <QTransform>
+#include "Blend.h"
+#include "Formula.h"
+#include "System.h"
+
+namespace chaoskit {
+namespace ui {
+
+ast::Blend toSource(const Blend *blend);
+ast::WeightedFormula toSource(const Formula *formula);
+ast::Transform toSource(const QTransform &transform);
+ast::System toSource(const models::System *system);
+
+}  // namespace ui
+}  // namespace chaoskit
+
+#endif  // CHAOSKIT_UI_TOSOURCE_H
