225348a6f3e6508e9cf978b602726d538b9c40f0
Rafał Hirsz <rafal@hirsz.co>
2019-05-19 22:04:25 +0200

    Add a fancy new toSource function that works with models

--- core/toSource.h
+++ core/toSource.h
@@ -4,6 +4,7 @@
 #include <ast/System.h>
 #include <ast/Transform.h>
 #include <ast/WeightedFormula.h>
+#include "models/Model.h"
 #include "structures/Blend.h"
 #include "structures/Formula.h"
 #include "structures/System.h"
@@ -15,6 +16,11 @@ ast::Blend toSource(const Blend &blend);
 ast::WeightedFormula toSource(const Formula &formula);
 ast::Transform toSource(const Transform &transform);
 ast::System toSource(const System &system);
+
+template <class T>
+auto toSource(const Model<T> &model) -> decltype(toSource(T())) {
+  return toSource(*model.data().lock());
+}
 
 }  // namespace chaoskit::core
 
--- lol.cpp
+++ lol.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-#include <vector>
-#include "core/models/ModelCollection.h"
+#include "core/models/DocumentModel.h"
 #include "core/models/SystemModel.h"
 #include "core/structures/System.h"
+#include "core/toSource.h"
+#include "core/transforms.h"
+#include "library/DeJong.h"
 
 using namespace chaoskit::core;
 
 int main(int argc, char** argv) {
-  std::vector<std::shared_ptr<System>> systems{};
-  ModelCollection<SystemModel> collection(systems);
+  DocumentModel document;
+  auto system = document.system();
+  system.finalBlend().setPost(translate(.5f, .5f) * scale(.5f, 1.f));
+
+  auto formula = system.blends().add().formulas().add();
+  formula.setSource(chaoskit::library::DeJong().source());
+  formula.setParams({9.379666578024626e-01f, 1.938709271140397e+00f,
+                     -1.580897020176053e-01f, -1.430070123635232e+00f});
+
+  std::cout << toSource(system) << std::endl;
 
   return 0;
 }
