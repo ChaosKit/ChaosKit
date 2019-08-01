04dfbd108a946b424a1dd665ab99aea017b6931c
Rafał Hirsz <rafal@hirsz.co>
2019-08-01 17:59:01 +0200

    Add explicit type casts from state::Id to uint64

--- state/Id.h
+++ state/Id.h
@@ -10,12 +10,19 @@ struct Id {
   uint32_t type = 0;
   uint32_t id = 0;
 
+  explicit Id(uint64_t value) : type(value >> 32u), id(value & 0xffffffff) {}
+  Id(uint32_t type, uint32_t id) : type(type), id(id) {}
+
   bool operator==(const Id& other) const noexcept {
     return other.type == type && other.id == id;
   }
 
   bool operator!=(const Id& other) const noexcept {
     return other.type != type || other.id != id;
+  }
+
+  explicit operator uint64_t() const {
+    return (static_cast<uint64_t>(type) << 32u) | id;
   }
 };
 
@@ -25,10 +32,10 @@ namespace std {
 template <>
 struct hash<chaoskit::state::Id> {
   using argument_type = chaoskit::state::Id;
-  using result_type = std::size_t;
+  using result_type = std::uint64_t;
 
   result_type operator()(const argument_type& id) const noexcept {
-    return (static_cast<result_type>(id.type) << 32u) | id.id;
+    return static_cast<result_type>(id);
   }
 };
 }  // namespace std
