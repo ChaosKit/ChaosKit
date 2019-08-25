ccc70df2652bb18b539a2ea044069a4e2c273b6b
Rafał Hirsz <rafal@hirsz.co>
2019-08-25 18:39:15 +0200

    Remove the GlobalStore

--- ui/modeltest.cpp
+++ ui/modeltest.cpp
@@ -1,19 +1,11 @@
 #include <QApplication>
-#include <QTimer>
-#include "GlobalStore.h"
 #include "ModelTestWindow.h"
 
-using chaoskit::ui::GlobalStore;
 using chaoskit::ui::ModelTestWindow;
 
 int main(int argc, char* argv[]) {
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QApplication app(argc, argv);
-
-  auto* timer = new QTimer();
-  QObject::connect(timer, &QTimer::timeout, &GlobalStore::get(),
-                   &GlobalStore::propagateChanges);
-  timer->start();
 
   auto* window = new ModelTestWindow;
   window->show();
