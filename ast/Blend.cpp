fdbe463f1960aeff4970ed09277b416a16df4836
Rafał Hirsz <rafal@hirsz.co>
2020-02-08 16:39:50 +0100

    Add support for coloring methods for blends in the AST
    Coloring methods are expressions that output a single number from 0-1 that is then later mapped to a color using a palette.

--- ast/Blend.cpp
+++ ast/Blend.cpp
@@ -7,7 +7,11 @@ GENERATE_NODE_TYPE(Blend);
 
 std::ostream &operator<<(std::ostream &stream, const Blend &blend) {
   stream << node_type(blend) << " (" << indent << indentation
-         << "pre: " << blend.pre() << indentation << "post: " << blend.post();
+         << "pre: " << blend.pre() << indentation << "post: " << blend.post()
+         << indentation << "coloring method:" << indent << indentation;
+
+  apply_visitor(StreamPrinter(stream), blend.coloringMethod());
+  stream << outdent;
 
   for (const auto &formula : blend.formulas()) {
     stream << indentation << formula;
--- ast/Blend.h
+++ ast/Blend.h
@@ -2,6 +2,7 @@
 #define CHAOSKIT_AST_BLEND_H
 
 #include <vector>
+#include "Expression.h"
 #include "Transform.h"
 #include "WeightedFormula.h"
 
@@ -15,21 +16,31 @@ class Blend {
   Blend(std::vector<WeightedFormula> formulas, const Transform &pre,
         const Transform &post)
       : formulas_(std::move(formulas)), pre_(pre), post_(post) {}
+  Blend(std::vector<WeightedFormula> formulas, const Transform &pre,
+        const Transform &post, const Expression &coloringMethod)
+      : formulas_(std::move(formulas)),
+        pre_(pre),
+        post_(post),
+        coloringMethod_(coloringMethod) {}
 
   [[nodiscard]] const std::vector<WeightedFormula> &formulas() const {
     return formulas_;
   }
   [[nodiscard]] const Transform &pre() const { return pre_; }
   [[nodiscard]] const Transform &post() const { return post_; }
+  [[nodiscard]] const Expression &coloringMethod() const {
+    return coloringMethod_;
+  }
 
   bool operator==(const Blend &other) const {
     return formulas_ == other.formulas_ && pre_ == other.pre_ &&
-           post_ == other.post_;
+           post_ == other.post_ && coloringMethod_ == other.coloringMethod_;
   }
 
  private:
   std::vector<WeightedFormula> formulas_;
   Transform pre_, post_;
+  Expression coloringMethod_ = 0.5f;
 };
 
 std::ostream &operator<<(std::ostream &stream, const Blend &blend);
