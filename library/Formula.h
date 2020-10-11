5ad35a3de2806d8a136c63525e508212540eb120
Rafał Hirsz <rafal@hirsz.co>
2020-10-11 20:28:55 +0200

    Add a helper to declare variables in formulas

--- library/Formula.h
+++ library/Formula.h
@@ -19,6 +19,32 @@ class Formula {
   ast::helpers::InputHelper input;
   ast::helpers::OutputHelper output;
   ast::helpers::ParameterHelper params;
+
+  class VariableHelper {
+   public:
+    VariableHelper(Formula* formula, std::string name)
+        : formula_(formula), name_(std::move(name)) {}
+
+    VariableHelper& operator=(ast::Expression expression) {
+      formula_->variables_.emplace_back(name_, std::move(expression));
+      return *this;
+    }
+
+   private:
+    Formula* formula_;
+    std::string name_;
+  };
+
+  VariableHelper var(std::string name) {
+    return VariableHelper(this, std::move(name));
+  }
+  [[nodiscard]] ast::Formula withVariables(
+      const ast::Expression (&list)[2]) const {
+    return ast::Formula(list[0], list[1], variables_);
+  }
+
+ private:
+  std::vector<ast::VariableDeclaration> variables_;
 };
 
 }  // namespace chaoskit::library
