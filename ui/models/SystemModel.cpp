c8fe9a86a5e72a9cdfc8a2b4dac754bf2ea01fc2
Rafał Hirsz <rafal@hirsz.co>
2021-04-19 19:30:01 +0200

    Make BlendModels accessible from the SystemModel

--- ui/models/SystemModel.cpp
+++ ui/models/SystemModel.cpp
@@ -6,13 +6,33 @@
 
 namespace chaoskit::ui {
 
+SystemModel::SystemModel(ModelFactory* modelFactory, QObject* parent)
+    : QObject(parent), BaseModel<System>(), modelFactory_(modelFactory) {
+  blends_ = new QQmlObjectListModel<BlendModel>(this);
+}
+
+void SystemModel::setProto(System* proto) {
+  BaseModel::setProto(proto);
+
+  // Clear old blend models
+  blends_->clear();
+
+  // Create new blend models
+  QList<BlendModel*> newModels;
+  for (auto& blend : *proto_->mutable_blends()) {
+    auto* model = modelFactory_->createBlendModel(blends_);
+    model->setProto(&blend);
+    newModels.append(model);
+  }
+  blends_->append(newModels);
+}
+
 QString SystemModel::astSource() const {
   std::stringstream stream;
   const core::CameraSystem& cameraSystem = toCameraSystem(*proto_);
   stream << cameraSystem;
   return QString::fromStdString(stream.str());
 }
-
 QString SystemModel::modelSource() const {
   std::string output;
   google::protobuf::TextFormat::PrintToString(*proto_, &output);
--- ui/models/SystemModel.h
+++ ui/models/SystemModel.h
@@ -1,27 +1,40 @@
 #ifndef CHAOSKIT_UI_MODELS_SYSTEMMODEL_H
 #define CHAOSKIT_UI_MODELS_SYSTEMMODEL_H
 
+#include <QQmlObjectListModel.h>
 #include <chaoskit.pb.h>
 #include <QObject>
 #include "BaseModel.h"
+#include "BlendModel.h"
+#include "ModelFactory.h"
 
 namespace chaoskit::ui {
 
 class SystemModel : public QObject, public BaseModel<System> {
   Q_OBJECT
+  Q_PROPERTY(QQmlObjectListModelBase* blends READ blends CONSTANT);
   Q_PROPERTY(const QString& astSource READ astSource NOTIFY somethingChanged);
   Q_PROPERTY(
       const QString& modelSource READ modelSource NOTIFY somethingChanged);
 
  public:
-  SystemModel(QObject* parent = nullptr)
-      : QObject(parent), BaseModel<System>() {}
+  SystemModel(ModelFactory* modelFactory, QObject* parent = nullptr);
+
+  void setProto(System* proto) override;
+
+  [[nodiscard]] QQmlObjectListModel<BlendModel>* blends() const {
+    return blends_;
+  }
 
   [[nodiscard]] QString astSource() const;
   [[nodiscard]] QString modelSource() const;
 
  signals:
   void somethingChanged();
+
+ private:
+  ModelFactory* modelFactory_;
+  QQmlObjectListModel<BlendModel>* blends_;
 };
 
 }  // namespace chaoskit::ui
