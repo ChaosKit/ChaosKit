b15946b7ef6b28fd2e65347e194ee7fdb361f9cd
Rafał Hirsz <rafal@hirsz.co>
2021-05-06 22:30:32 +0200

    Replace randutils with PCG
    randutils was causing problems on Apple Silicon.

--- core/ThreadLocalRng.cpp
+++ core/ThreadLocalRng.cpp
@@ -1,21 +1,25 @@
 #include "ThreadLocalRng.h"
 
-#include "randutils.hpp"
+#include <pcg_random.hpp>
+#include <random>
 
 namespace chaoskit::core {
 
 namespace {
-thread_local randutils::mt19937_rng rng;
-}
+thread_local pcg32 rng{pcg_extras::seed_seq_from<std::random_device>()};
+}  // namespace
 
 double ThreadLocalRng::randomDouble(double min, double max) {
-  return rng.uniform(min, max);
+  std::uniform_real_distribution<double> distribution(min, max);
+  return distribution(rng);
 }
 float ThreadLocalRng::randomFloat(float min, float max) {
-  return rng.uniform(min, max);
+  std::uniform_real_distribution<float> distribution(min, max);
+  return distribution(rng);
 }
 int ThreadLocalRng::randomInt(int min, int max) {
-  return rng.uniform(min, max);
+  std::uniform_int_distribution<int> distribution(min, max);
+  return distribution(rng);
 }
 
 }  // namespace chaoskit::core
